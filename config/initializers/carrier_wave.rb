78e4a54fcb9e1bda3416e88813e9fe60d6689f60
EC2 Default User <ec2-user@ip-172-31-21-5.ec2.internal>
2019-01-02 19:28:48 +0000

    updated for fog-aws

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,7 +1,14 @@
+require 'carrierwave/storage/abstract' # Add this line <<<<<
+require 'carrierwave/storage/file' # Add this line <<<<<
+require 'carrierwave/storage/fog' # Add this line <<<<<
+ 
 if Rails.env.production?
   CarrierWave.configure do |config|
+    config.storage = :fog # Add this line <<<<<
+    
+    config.fog_provider = 'fog/aws'
     config.fog_credentials = {
-      :provider => 'AWS', 
+      :provider => 'AWS',
       :aws_access_key_id => ENV['S3_ACCESS_KEY'],
       :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
